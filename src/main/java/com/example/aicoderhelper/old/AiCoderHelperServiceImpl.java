c43e5dcd11a7252ab4ffe59acfa8717cf6bc0565
segho <15608624936@163.com>
2026-04-24 16:19:36 +0800

    Revert "delete impl.java"
    This reverts commit bd40df2dc4d85e107b00f0d101f4a87bdab4ec96.

---
+++ src/main/java/com/example/aicoderhelper/old/AiCoderHelperServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.aicoderhelper.old;
+
+import com.example.aicoderhelper.ai.AiCoderHelperService;
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.chat.response.ChatResponse;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class AiCoderHelperServiceImpl implements AiCoderHelperService {
+
+
+    @Resource
+    private ChatModel qwenChatmodel;
+
+    @Override
+    public String chatM(String msg) {
+        UserMessage userMessage = UserMessage.from(msg);
+        ChatResponse chatResponse = qwenChatmodel.chat(userMessage);
+        AiMessage aiMessage = chatResponse.aiMessage();
+        log.info("Impl response: \n" + aiMessage.text());
+        return "";
+    }
+}
